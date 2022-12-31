d0eeded60542ff9c9a79930346bc86e0ff0fa60a
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-12-31 20:38:31 +0600

    data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleCard from "./SingleCard";
 
 const Card = () => {
+  const [dataList, setDataList] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setDataList(data));
+  }, []);
+
   return (
     <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
       {/* <!-- single card  --> */}
-      <SingleCard />
-      {/* <!-- single card  --> */}
-      <SingleCard />
-
-      {/* <!-- single card  --> */}
-      <SingleCard />
-
-      {/* <!-- single card  --> */}
-      <SingleCard />
-      {/* <!-- single card  --> */}
-      <SingleCard />
-
-      {/* <!-- single card  --> */}
-      <SingleCard />
+      {dataList.map((data) => (
+        <SingleCard data={data}></SingleCard>
+      ))}
     </div>
   );
 };
