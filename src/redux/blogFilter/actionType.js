b30469243a25c57dc0506ebdf741045a784931a2
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-12-31 21:00:53 +0600

    filter

---
+++ src/redux/blogFilter/actionType.js
@@ -0,0 +1,4 @@
+const SEARCHFILTER = "action/searchFilter";
+const CATEGORYFILTER = "action/categoryFilter";
+const AUTHORIMGFILTER = "action./authorImgFilter";
+const AUTHORFILTER = "action/authorFilter";
---
+++ src/redux/blogFilter/initialState.js
@@ -0,0 +1,6 @@
+const initialState = {
+  search: "",
+  category: "",
+  Author: "",
+  AuthorImg: "",
+};
