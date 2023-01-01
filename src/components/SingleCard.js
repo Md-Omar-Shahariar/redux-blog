4232a20ddf8054c78a72191da93d547813b32706
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-01 20:28:09 +0600

    hover

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -22,15 +22,15 @@ const SingleCard = ({ data }) => {
         <div class="flex-1 ">
           <p
             onClick={() => dispatch(category(cate))()}
-            class="text-sm font-medium text-indigo-600 hover:pointer"
+            class="text-sm font-medium text-indigo-600 hover:cursor-pointer"
           >
             <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">
               {cate}
             </span>
           </p>
-          <a href="#" class="block mt-1">
+          <div href="" class="block mt-1">
             <p class="text-xl font-semibold text-gray-900">{title}</p>
-          </a>
+          </div>
         </div>
         <div class="mt-6 flex items-center">
           <div
@@ -40,7 +40,7 @@ const SingleCard = ({ data }) => {
             <img class="h-10 w-10 rounded-full" src={authImg} alt="" />
           </div>
           <div class="ml-3" onClick={() => dispatch(authorSearch(auth))()}>
-            <p class="text-sm font-medium text-gray-900 hover:underline">
+            <p class="text-sm font-medium text-gray-900 hover:cursor-pointer">
               {auth}
             </p>
             <div class="flex space-x-1 text-sm text-gray-500">
